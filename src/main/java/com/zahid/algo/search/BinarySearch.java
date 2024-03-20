b1476768fad34ccc5adec19c5e2df57fa2ade4e4
zahid <zahid.aiub6@gmail.com>
2024-03-21 00:31:24 +0100

    project init with gitignore

---
+++ src/main/java/com/zahid/algo/search/BinarySearch.java
@@ -0,0 +1,7 @@
+package com.zahid.algo.search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.printf("Binary Search");
+    }
+}
---
+++ src/main/java/com/zahid/algo/sort/MergeSort.java
@@ -0,0 +1,7 @@
+package com.zahid.algo.sort;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        System.out.printf("Merge Sort");
+    }
+}
